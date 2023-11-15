8829f06fa8b8d572ba769d2e767d9ffbd02b5ac5
= envelopingCODE <>
2023-11-15 20:58:40 +0100

    Carified

--- mission.js
+++ mission.js
@@ -39,16 +39,16 @@ missionButtonEl.addEventListener('mousedown', function(){
     addMission(sanitizedInput); });                       // After which the addMission function will be called with the sanitized text
 
 
-    // Special Event listener for ENTER to sanitize inputfield
+    // Special Event listener for ENTER + sanitize inputfield
 
     inputEl.addEventListener('keypress', function(e) { 
         if (e.key === 'Enter') {
             const input = inputEl.value;                          // input equals the value of the textfield
             const sanitizedInput = sanitizeInput(input);         // SanitizedInput declared as the cleaned text after the sanitizeInput function has run 
-            addMission(sanitizedInput);                       // After which the addMission function will be called with the sanitized text
+            addMission(sanitizedInput); } });                      // After which the addMission function will be called with the sanitized text
         
-      }
-    });
+
+   
     
         
 
